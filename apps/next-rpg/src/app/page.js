66cce3877c64d53a3822bfc43979534c1b2b65af
ajmarvingomez <ajmarvingomez@gmail.com>
2025-02-13 23:49:38 -0500

    feat: Add character creation function

--- apps/next-rpg/src/app/page.js
+++ apps/next-rpg/src/app/page.js
@@ -24,7 +24,7 @@ function Input({ id, title }) {
   return (
     <div>
       <label htmlFor={id}>{title}</label>
-      <input id={id} />
+      <input id={id} name={id} />
     </div>
   );
 }
@@ -33,7 +33,7 @@ function Select({ id, title, list }) {
   return (
     <div>
       <label htmlFor={id}>{title}</label>
-      <select id={id}>
+      <select id={id} name={id}>
         {list.map((option, index) => {
           return (
             <option key={index} value={option.slug}>
@@ -49,6 +49,21 @@ function Select({ id, title, list }) {
 export default function Home() {
   const [player, setPlayer] = useState(null);
   const [openForm, setOpenForm] = useState(false);
+  function createCharacter(formData) {
+
+    const name = formData.get('name')
+    const species = formData.get('species')
+    const _class = formData.get('class')
+    console.log(formData)
+    setPlayer(
+      {
+        ...player,
+        'name': name,
+        'species': species,
+        'class': _class
+      }
+    )
+  }
   return (
     <div className={styles.page}>
       <header></header>
@@ -60,7 +75,7 @@ export default function Home() {
           </button>
         )}
         {openForm && (
-          <form>
+          <form action={createCharacter}>
             <Input id="name" title="Name" />
             <Select id="species" title="Species" list={species} />
             <Select id="class" title="Class" list={classes} />
