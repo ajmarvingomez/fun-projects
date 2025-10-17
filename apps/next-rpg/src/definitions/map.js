beb96663c0143abd15e74089a11954991ae63532
ajmarvingomez <ajmarvingomez@gmail.com>
2025-10-16 20:57:11 -0400

    feature: Create terrain definitions

--- apps/next-rpg/src/definitions/map.js
+++ apps/next-rpg/src/definitions/map.js
@@ -1,2 +1,11 @@
 export const mapHeight = 10;
 export const mapWidth = 10;
+
+export const terrainDefinitions = [
+    {
+        slug: "grass",
+        name: "Grass",
+        description: "A patch of grass.",
+        walkable: true,
+    }
+]
