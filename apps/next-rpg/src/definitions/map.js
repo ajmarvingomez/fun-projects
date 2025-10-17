1c0398b12dac6594741143e25edb9fc2da7aedf0
ajmarvingomez <ajmarvingomez@gmail.com>
2025-10-16 21:19:52 -0400

    feature: Add dirt definition feature: Add mountain definition

--- apps/next-rpg/src/definitions/map.js
+++ apps/next-rpg/src/definitions/map.js
@@ -7,5 +7,17 @@ export const terrainDefinitions = [
         name: "Grass",
         description: "A patch of grass.",
         walkable: true,
+    },
+    {
+        slug: "mountain",
+        name: "Mountain",
+        description: "A tall mountain.",
+        walkable: false,
+    },
+    {
+        slug: "dirt",
+        name: "Dirt",
+        description: "A patch of dirt.",
+        walkable: true,
     }
 ]
