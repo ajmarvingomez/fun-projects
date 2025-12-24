de4d5edeae4f3e31d12ab91e8b645b68fdce7d72
ajmarvingomez <ajmarvingomez@gmail.com>
2025-12-24 11:34:47 -0500

    feature: Ability to launch game.

--- apps/battlemans/src/App.jsx
+++ apps/battlemans/src/App.jsx
@@ -1,18 +1,48 @@
-
+import { useEffectEvent, useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false)
   function startGame() {
     console.log("Starting Game...")
+    setIsGameStarted(true)
   }
+  const gameIsStarted = useEffectEvent(gameState => addToGameLog("Game Started:", gameState))
+  useEffect(() => {
+      if( isGameStarted ) gameIsStarted(isGameStarted)
+    }, [isGameStarted])
+
+
+  const [gameLog, setGameLog] = useState([])
+
+  function addToGameLog(entry) {
+    setGameLog(prevLog => [...prevLog, entry])
+  }
+
 
   return (
     <>
       <h1>BattleMans</h1>
-      <button onClick={() => startGame()}>Start Game</button>
-      <div>
+      {
+        !isGameStarted && <button onClick={() => startGame()}>Start Game</button>
+      }
+      {
+        isGameStarted && <div className='game-container'>
+          <div className='sidebar'>
+            <h2>Sidebar</h2>
+          </div>
+          <div className='game-area'>
+            <h2>Game Area</h2>
+          </div>
+          <div className='game-log'>
+            <h2>Game Log</h2>
+            {gameLog.map((logEntry, index) => (
+              <div key={index}>{logEntry}</div>
+            ))}
+          </div>
+        </div>
+      }
 
-      </div>
     </>
   )
 }
